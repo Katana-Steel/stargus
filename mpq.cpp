b2fd8564a950f80aed17ea16bd5d32123890d3de
Rene Kjellerup <rk.katana.steel@gmail.com>
2017-05-23 14:02:50 -0700

    added logging without changing behavior
    output:
    extracted: tileset\Jungle.wpe (1811, 1024 bytes)
    Warning file: tileset\Jungle.vr4
    read size:5004 bytes at block: 74 metod found: '
    extracted: tileset\Jungle.vr4 (1809, 1524352 bytes)
    extracted: tileset\Jungle.vx4 (1810, 161472 bytes)
    extracted: tileset\Jungle.cv5 (1806, 82056 bytes)
    extracted: tileset\Jungle.vf4 (1808, 161472 bytes)
    
    I've tried a few thing to get it to output the metod variable
    in some readable form...

--- mpq.cpp
+++ mpq.cpp
@@ -51,6 +51,7 @@
 #include <unistd.h>
 #endif
 #include <ctype.h>
+#include <iostream>
 
 #include "mpq.h"
 #include "huff.h"
@@ -577,6 +578,11 @@ int CMpq::ExtractTo(unsigned char *mpqbuf, UInt32 entry, FILE *fpMpq)
 						ExtWavUnp3((UIntPtr) read_buffer,
 						(UIntPtr) length_read, (UIntPtr) write_buffer,
 						0x1000);
+				}
+				if (length_read > 0x1000) {
+					std::cout << "Warning file: " << this->FilenameTable+(entry * PATH_MAX) << '\n';
+					std::cout << "read size:" << length_read << " bytes at block: ";
+					std::cout << j << " metod found: '" << std::hex << (uint8_t)metod << "'" << std::endl;
 				}
 				memcpy(mpqptr, write_buffer, length_read);
 				mpqptr += length_read;
